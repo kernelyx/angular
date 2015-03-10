e37cd676e300194242478f31bdd4f018aa431b7e
Victor <hanweihw@gmail.com>
2015-03-10 11:21:21 -0700

    beta version, but I like it.

---
+++ app.js
@@ -0,0 +1,83 @@
+//setup mongoose database
+require( './db' );
+
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+var engine = require('ejs-locals');
+var routes = require('./routes');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//In order to use the layout
+app.engine('ejs', engine);
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/', routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.get('/', routes.index);
+app.post('/create', routes.create);
+app.get('/destory/:id', routes.destory);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
+app.get('/completed/:id', routes.completed);
+app.get('/checktodo/:id', routes.checktodo);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example server listening at http://%s:%s', host, port)
+})
+
+//Another way to monitor
+// var http = require( 'http' );
+// app.set('port', process.env.PORT || 3001);
+// http.createServer( app ).listen( app.get( 'port' ), function (){
+//   console.log( 'Express server listening on port ' + app.get( 'port' ));
+// });
---
+++ db.js
@@ -0,0 +1,13 @@
+var mongoose = require( 'mongoose' );
+var Schema   = mongoose.Schema;
+
+var Todo = new Schema({
+    user_id    : String,
+    content    : String,
+    updated_at : Date,
+    status : String
+});
+
+mongoose.model( 'Todo', Todo );
+
+mongoose.connect( 'mongodb://localhost/express-todo' );
